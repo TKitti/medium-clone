9ac1c9a4a478c0faea63814e2ffd519882129597
Kitti <kittitoth8@gmail.com>
2023-04-02 20:09:38 +0200

    register component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from "@angular/forms";
+
+const routes = [
+  {
+    path: "register",
+    component: RegisterComponent
+  }
+]
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule], 
+  declarations: [
+    RegisterComponent
+  ]
+})
+export class AuthModule{}
