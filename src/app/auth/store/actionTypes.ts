8b9321671d36376b3c7f79f0a8dd2034fbb9edb9
Kitti <kittitoth8@gmail.com>
2023-04-02 21:20:28 +0200

    create action types for authentication module store

---
+++ src/app/auth/store/actionTypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  REGISTER = '[Auth] Register',  // the name in the square brackets always refers to the module name we are in
+  REGISTER_SUCCESS = '[Auth] Register success',
+  REGISTER_FAILURE = '[Auth] Register failure'
+}
